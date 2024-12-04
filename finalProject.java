c434ebebcb3d79ce63dd16522ac773185c7d973a
BillyBoBoi <mcge0256@bears.unco.edu>
2024-12-04 15:03:52 -0700

    Update finalProject.java

--- finalProject.java
+++ finalProject.java
@@ -48,21 +48,45 @@ public static int recursionSpell(int damageLevel, int baseDamage){
 public static void main(String args[]) throws IOException{
         
     Scanner scnr = new Scanner(System.in);
+    int i;
+    final int BAG_SIZE = 10;
+    String[] bagItemNames = new String[BAG_SIZE];
+    int[] bagItemStat = new int[BAG_SIZE];
     
+    ArrayList<String> potionName = new ArrayList<String>();
+    ArrayList<Integer> potionStat = new ArrayList<Integer>();
+    ArrayList<String> physicalName = new ArrayList<String>();
+    ArrayList<Integer> physicalStat = new ArrayList<Integer>();
+    ArrayList<String> manaName = new ArrayList<String>();
+    ArrayList<Integer> manaStat = new ArrayList<Integer>();
+    ArrayList<Integer> manaCost = new ArrayList<Integer>();
     
     String userPath;
     
-    FileInputStream itemFile = new FileInputStream("potionFile.txt");
-    Scanner fileRead = new Scanner(itemFile);
+    FileInputStream potionFile = new FileInputStream("potionFile.txt");
+    Scanner potionRead = new Scanner(potionFile);
+    FileInputStream physicalFile = new FileInputStream("physicalFile.txt");
+    Scanner physicalRead = new Scanner(physicalFile);
+    FileInputStream manaFile = new FileInputStream("manaFile.txt");
+    Scanner manaRead = new Scanner(manaFile);
+    
+    
     System.out.println("Hello adventurer! You are on a mightly quest to save the world! However, first you must pick a path; there are three diverging paths ahead, one forward, one to the right, and one to the left.");
     userPath = scnr.next();
 
-    while (){
-
+    while (potionRead.hasNext()){
+        potionName.add(potionRead.next());
+        potionStat.add(potionRead.nextInt());
     }
-        
-        
-        
-        
+    while (physicalRead.hasNext()){
+        physicalName.add(physicalRead.next());
+        physicalStat.add(physicalRead.nextInt());
     }
+    while (manaRead.hasNext()){
+        manaName.add(manaRead.next());
+        manaStat.add(manaRead.nextInt());
+        manaCost.add(manaRead.nextInt());
+    }
+    
+}
 }
